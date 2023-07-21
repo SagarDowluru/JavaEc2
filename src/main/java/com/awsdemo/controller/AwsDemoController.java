374572264a5cc3ba9bfe3c8a531da758b5f31b80
SagarDowluru <sagardolwuru@gnail.com>
2023-07-22 00:02:07 +0530

    First Commit

---
+++ src/main/java/com/awsdemo/controller/AwsDemoController.java
@@ -0,0 +1,18 @@
+package com.awsdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.service.annotation.GetExchange;
+
+@RestController
+public class AwsDemoController {
+
+	
+	
+	@GetMapping("/go")
+	public String getAccess() {
+		
+		return "Working fine.....!";
+		
+	}
+}
